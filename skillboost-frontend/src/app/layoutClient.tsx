56beb66b9a67f1929bd9fee4520ccc8519146c83
cvccvc40 <158179026+cvccvc40@users.noreply.github.com>
2025-02-27 00:21:49 +0300

    Added redux packages

--- skillboost-frontend/src/app/layoutClient.tsx
+++ skillboost-frontend/src/app/layoutClient.tsx
@@ -1,19 +1,23 @@
 "use client"
 
 import { ReactNode } from "react";
+import { Provider } from "react-redux";
+import { store } from "@/store/store";
 
 export default function RootLayoutClient({ children }: { children: ReactNode }) {
     return (
         <div>
-            <header>
-                <h1>Header</h1>
-            </header>
-            <main>
-                {children}
-            </main>
-            <footer>
-                <h1>Footer</h1>
-            </footer>
+            <Provider store={store}>
+                <header>
+                    <h1>Header</h1>
+                </header>
+                <main>
+                    {children}
+                </main>
+                <footer>
+                    <h1>Footer</h1>
+                </footer>
+            </Provider>
         </div>
     )
 }
--- skillboost-frontend/src/app/page.tsx
+++ skillboost-frontend/src/app/page.tsx
@@ -1,5 +1,5 @@
 
-import RootLayoutClient from "./layoutClient";
+import RootLayoutClient from "../app/layoutClient";
 import styles from "./page.module.css";
 
 export default function Home() {
