3cf2e5a87d10bc30d102a8fdea17fe5f95cfe820
Dasha <153850235+kowkaLYT@users.noreply.github.com>
2025-03-01 10:21:05 +0200

    Some changes

--- skillboost-frontend/src/widgets/Header/ui/Header.tsx
+++ skillboost-frontend/src/widgets/Header/ui/Header.tsx
@@ -1,5 +1,5 @@
+import Link from 'next/link'
 import React from 'react'
-
 interface HeaderProps {
   isAuthenticated: boolean
 }
@@ -7,7 +7,11 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ isAuthenticated }) => {
   return (
     <header className='header'>
-      <div className='header__logo'>Byway</div>
+      <div className='header__logo'>
+        <img src='/icons/icon-logo.png' alt='Byway' />
+        <p>Byway</p>
+      </div>
+      <Link href='/categories'>Categories</Link>
       <input type='text' placeholder='Search courses' />
       <span>Teach on Byway</span>
 
