7da2d44ff2ee8c1ec4f96732d8c56b1129d0901d
Dasha <153850235+kowkaLYT@users.noreply.github.com>
2025-03-01 10:47:22 +0200

    layout without style

--- skillboost-frontend/src/widgets/Header/ui/Header.tsx
+++ skillboost-frontend/src/widgets/Header/ui/Header.tsx
@@ -1,29 +1,58 @@
+import { Bell, Heart, Search, ShoppingCart } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
+import styles from './Header.module.scss'
 interface HeaderProps {
   isAuthenticated: boolean
 }
 
 const Header: React.FC<HeaderProps> = ({ isAuthenticated }) => {
   return (
-    <header className='header'>
-      <div className='header__logo'>
-        <img src='/icons/icon-logo.png' alt='Byway' />
-        <p>Byway</p>
+    <header>
+      <div className={styles.headerLogo}>
+        <img
+          className={styles.headerLogoIcon}
+          src='/icons/icon-logo.png'
+          alt='Byway'
+        />
+        <p className={styles.headerLogoText}>Byway</p>
       </div>
-      <Link href='/categories'>Categories</Link>
-      <input type='text' placeholder='Search courses' />
-      <span>Teach on Byway</span>
-
-      {isAuthenticated ? (
-        <div></div>
-      ) : (
-        <div>
-          <button>Log In</button>
-          <button>Sign Up</button>
-        </div>
-      )}
-      <p>Teach on Byway</p>
+      <nav className={styles.headerNav}>
+        <Link className={styles.headerNavLink} href='/categories'>
+          Categories
+        </Link>
+      </nav>
+      <div className={styles.headerSearch}>
+        <input
+          className={styles.headerSearchInput}
+          type='text'
+          placeholder='Search courses'
+          aria-label='Search courses'
+        />
+        <button className={styles.headerSearchButton}>
+          <Search color='#64748B' />
+        </button>
+      </div>
+      <span className={styles.headerTeach}>Teach on Byway</span>
+      <div className={styles.headerActions}>
+        {isAuthenticated ? (
+          <div className={styles.headerIcons}>
+            <Heart className={styles.headerIcon} color='#64748B' />
+            <ShoppingCart className={styles.headerIcon} color='#64748B' />
+            <Bell className={styles.headerIcon} color='#64748B' />
+          </div>
+        ) : (
+          <div className={styles.headerAuth}>
+            <ShoppingCart className={styles.headerIcon} color='#64748B' />
+            <Link href='/login'>
+              <button className={styles.headerAuthLogin}>Log In</button>
+            </Link>
+            <Link href='/signup'>
+              <button className={styles.headerAuthSignup}>Sign Up</button>
+            </Link>
+          </div>
+        )}
+      </div>
     </header>
   )
 }
