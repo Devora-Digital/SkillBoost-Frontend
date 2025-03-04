caffd70e75c15822cbde9350c6f5b4aeab740a1a
cvccvc40 <158179026+cvccvc40@users.noreply.github.com>
2025-03-04 21:04:59 +0300

    Edited statistic block on home page

--- skillboost-frontend/src/app/page.tsx
+++ skillboost-frontend/src/app/page.tsx
@@ -7,7 +7,27 @@ export default function Home() {
     <RootLayoutClient>
       <div className={styles.page}>
           <div>Banner</div>
-          <div>Statistic</div>
+          <div className={styles.statistic_block}>
+              <div className={styles.inside_block}>
+                  <p>250+</p>
+                  <p>Courses by our best mentors</p>
+              </div>
+              <div className={styles.vertical_line}></div>
+              <div className={styles.inside_block}>
+                  <p>1000+</p>
+                  <p>Courses by our best mentors</p>
+              </div>
+              <div className={styles.vertical_line}></div>
+              <div className={styles.inside_block}>
+                  <p>15+</p>
+                  <p>Courses by our best mentors</p>
+              </div>
+              <div className={styles.vertical_line}></div>
+              <div className={styles.inside_block}>
+                  <p>2400+</p>
+                  <p>Courses by our best mentors</p>
+              </div>
+          </div>
           <div>Top Categories</div>
           <div>Top Courses</div>
           <div>Top Instructors</div>
