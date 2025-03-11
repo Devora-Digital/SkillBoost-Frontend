5fdc3baaadd8446a98efce2e04b7bb41e6daafe7
cvccvc40 <158179026+cvccvc40@users.noreply.github.com>
2025-03-11 19:04:32 +0300

    Added gap for quote items and some items

--- skillboost-frontend/src/app/page.tsx
+++ skillboost-frontend/src/app/page.tsx
@@ -314,6 +314,28 @@ export default function Home() {
                         </div>
                     </div>
                 </div>
+                <div className={styles.quotes_block_quotes_item}>
+                    <Image src={QuoteMarkIcon} alt="Quote mark icon" />
+                    <p className={styles.quotes_block_quote_text}>&quot;Byway&apos;s tech courses are top-notch! As someone who&apos;s always looking to stay ahead in the rapidly evolving tech world, I appreciate the up-to-date content and engaging multimedia.</p>
+                    <div className={styles.quotes_block_author}>
+                        <Image src={DesignerPhoto} className={styles.quotes_block_author_photo} alt="Designer photo"/>
+                        <div className={styles.quotes_block_author_inside}>
+                            <p className={styles.quotes_block_author_name}>Jane Doe</p>
+                            <p className={styles.quotes_block_author_post}>Designer</p>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.quotes_block_quotes_item}>
+                    <Image src={QuoteMarkIcon} alt="Quote mark icon" />
+                    <p className={styles.quotes_block_quote_text}>&quot;Byway&apos;s tech courses are top-notch! As someone who&apos;s always looking to stay ahead in the rapidly evolving tech world, I appreciate the up-to-date content and engaging multimedia.</p>
+                    <div className={styles.quotes_block_author}>
+                        <Image src={DesignerPhoto} className={styles.quotes_block_author_photo} alt="Designer photo"/>
+                        <div className={styles.quotes_block_author_inside}>
+                            <p className={styles.quotes_block_author_name}>Jane Doe</p>
+                            <p className={styles.quotes_block_author_post}>Designer</p>
+                        </div>
+                    </div>
+                </div>
             </div>
           </div>
           <div>Promotions</div>
