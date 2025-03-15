25d2749e1216bfc3af3d9566982ee12b12788c18
cvccvc40 <158179026+cvccvc40@users.noreply.github.com>
2025-03-15 23:08:19 +0300

    Edited banner pictures position 3

--- skillboost-frontend/src/app/page.tsx
+++ skillboost-frontend/src/app/page.tsx
@@ -37,17 +37,17 @@ export default function Home() {
                 <button className={styles.banner_block_button}>Start your instructor journey</button>
             </div>
             <div className={styles.banner_block_inside_block_second}>
-                <div>
+                <div className={styles.wrapper_circle_first}>
                     <div className={styles.banner_circle_first}>
                         <Image className={styles.banner_image_first} src={BannerImageFirst} alt="Banner image 01" width={183.62} height={260.32} />
                     </div>
                 </div>
-                <div>
+                <div className={styles.wrapper_circle_second}>
                     <div className={styles.banner_circle_second}>
                         <Image className={styles.banner_image_second} src={BannerImageSecond} alt="Banner image 02" width={173.88} height={165.19} />
                     </div>
                 </div>
-                <div>
+                <div className={styles.wrapper_circle_third}>
                     <div className={styles.banner_circle_third}>
                         <Image className={styles.banner_image_third} src={BannerImageThird} alt="Banner image 03" width={331.64} height={221.09} />
                     </div>
