2cdbf8b56dc33156758e7622576234a38e2240f4
Dasha <153850235+kowkaLYT@users.noreply.github.com>
2025-03-15 23:21:51 +0200

    Fix styles and Next Image

--- skillboost-frontend/src/widgets/Login/ui/Login.tsx
+++ skillboost-frontend/src/widgets/Login/ui/Login.tsx
@@ -1,69 +1,81 @@
 import { ArrowRight } from 'lucide-react'
 import styles from './Login.module.scss'
+import Image from 'next/image'
 
 const Login = () => {
   return (
-    <div className={styles.container}>
-      <form className={styles.containerForm}>
-        <h2 className={styles.formTitle}>Sign in to your account</h2>
+    <div className={styles.SignIn}>
+      <div className={styles.container}>
+        <h2 className={styles.containerTitle}>Sign in to your account</h2>
         <div>
-          <h5 className={styles.formSection}>Email</h5>
+          <h5 className={styles.containerSection}>Email</h5>
           <input
-            className={styles.formInput}
+            className={styles.containerInput}
             type='text'
             placeholder='Username or Email ID'
           />
         </div>
+
         <div>
-          <h5 className={styles.formSection}>Password</h5>
+          <h5 className={styles.containerSection}>Password</h5>
           <input
-            className={styles.formInput}
+            className={styles.containerInput}
             type='password'
             placeholder='Enter Password'
           />
         </div>
-        <button type='submit' className={styles.formButton}>
+
+        <button type='submit' className={styles.containerButton}>
           <span>Sign In</span>
-          <ArrowRight />
+          <ArrowRight size={16} />
         </button>
 
         <div className={styles.signWith}>
           <div className={styles.signWithLine}></div>
-          <p className={styles.signWithText}>Sign up with</p>
+          <p className={styles.signWithText}>Sign in with</p>
           <div className={styles.signWithLine}></div>
         </div>
 
         <ul className={styles.socialsBox}>
           <li className={styles.social}>
-            <img
+            <Image
               src='/icons/icon-fb.svg'
               alt='Facebook'
               className={styles.socialIcon}
+              width={24}
+              height={24}
             />
             <p style={{ color: '#0866FF' }}>Facebook</p>
           </li>
           <li className={styles.social}>
-            <img
+            <Image
               src='/icons/icon-google.svg'
               alt='Google'
               className={styles.socialIcon}
+              width={24}
+              height={24}
             />
             <p style={{ color: '#EA4335' }}>Google</p>
           </li>
           <li className={styles.social}>
-            <img
+            <Image
               src='/icons/icon-microsoft.svg'
               alt='Microsoft'
               className={styles.socialIcon}
+              width={24}
+              height={24}
             />
             <p style={{ color: '#000000' }}>Microsoft</p>
           </li>
         </ul>
-      </form>
-      <img
+      </div>
+      <Image
         className={styles.Image}
         src='/assets/img-login.png'
         alt='SkillBoost'
+        width={625}
+        height={934}
+        priority
       />
     </div>
   )
--- skillboost-frontend/src/widgets/Registration/ui/Registration.tsx
+++ skillboost-frontend/src/widgets/Registration/ui/Registration.tsx
@@ -1,108 +1,118 @@
 import { ArrowRight } from 'lucide-react'
 import styles from './Registration.module.scss'
+import Image from 'next/image'
 
 export default function Registration() {
   return (
-    <div className='container'>
-      <div className={styles.SignUp}>
-        <div className={styles.Image}>
-          <img src='/assets/img-signup.png' alt='SkillBoost' />
-        </div>
-        <div className={styles.container}>
-          <h2 className={styles.containerTitle}>Create Your Account</h2>
-          <div className={styles.containerLine}>
-            <div className={styles.block}>
-              <h5 className={styles.blockTitle}>Full Name</h5>
-              <div className={styles.blockFlex}>
-                <input
-                  className={styles.blockInput}
-                  type='text'
-                  placeholder='First Name'
-                />
-                <input
-                  className={styles.blockInput}
-                  type='text'
-                  placeholder='Last Name'
-                />
-              </div>
-            </div>
-          </div>
-
+    <div className={styles.SignUp}>
+      <div className={styles.Image}>
+        <Image
+          src='/assets/img-signup.png'
+          alt='SkillBoost'
+          width={625}
+          height={934}
+        />
+      </div>
+      <div className={styles.container}>
+        <h2 className={styles.containerTitle}>Create Your Account</h2>
+        <div className={styles.containerLine}>
           <div className={styles.block}>
-            <h5 className={styles.blockTitle}>Username</h5>
-            <input
-              type='text'
-              placeholder='Username'
-              className={styles.blockInput}
-            />
-          </div>
-
-          <div className={styles.block}>
-            <h5 className={styles.blockTitle}>Email</h5>
-            <input
-              type='email'
-              placeholder='Email ID'
-              className={styles.blockInput}
-            />
-          </div>
-
-          <div className={styles.containerLine}>
-            <div className={styles.block}>
-              <h5 className={styles.blockTitle}>Password</h5>
+            <h5 className={styles.blockTitle}>Full Name</h5>
+            <div className={styles.blockFlex}>
               <input
                 className={styles.blockInput}
-                type='password'
-                placeholder='Enter Password'
+                type='text'
+                placeholder='First Name'
               />
-            </div>
-            <div className={styles.block}>
-              <h5 className={styles.blockTitle}>Confirm Password</h5>
               <input
                 className={styles.blockInput}
-                type='password'
-                placeholder='Confirm Password'
+                type='text'
+                placeholder='Last Name'
               />
             </div>
           </div>
-
-          <button type='submit' className={styles.Button}>
-            <span>Sign Up</span>
-            <ArrowRight />
-          </button>
-
-          <div className={styles.signWith}>
-            <div className={styles.signWithLine}></div>
-            <p className={styles.signWithText}>Sign up with</p>
-            <div className={styles.signWithLine}></div>
-          </div>
-
-          <ul className={styles.socialsLine}>
-            <li className={styles.social}>
-              <img
-                src='/icons/icon-fb.svg'
-                alt='Facebook'
-                className={styles.socialIcon}
-              />
-              <p style={{ color: '#0866FF' }}>Facebook</p>
-            </li>
-            <li className={styles.social}>
-              <img
-                src='/icons/icon-google.svg'
-                alt='Google'
-                className={styles.socialIcon}
-              />
-              <p style={{ color: '#EA4335' }}>Google</p>
-            </li>
-            <li className={styles.social}>
-              <img
-                src='/icons/icon-microsoft.svg'
-                alt='Microsoft'
-                className={styles.socialIcon}
-              />
-              <p style={{ color: '#000000' }}>Microsoft</p>
-            </li>
-          </ul>
         </div>
+
+        <div className={styles.block}>
+          <h5 className={styles.blockTitle}>Username</h5>
+          <input
+            type='text'
+            placeholder='Username'
+            className={styles.blockInput}
+          />
+        </div>
+
+        <div className={styles.block}>
+          <h5 className={styles.blockTitle}>Email</h5>
+          <input
+            type='email'
+            placeholder='Email ID'
+            className={styles.blockInput}
+          />
+        </div>
+
+        <div className={styles.containerLine}>
+          <div className={styles.block}>
+            <h5 className={styles.blockTitle}>Password</h5>
+            <input
+              className={styles.blockInput}
+              type='password'
+              placeholder='Enter Password'
+            />
+          </div>
+          <div className={styles.block}>
+            <h5 className={styles.blockTitle}>Confirm Password</h5>
+            <input
+              className={styles.blockInput}
+              type='password'
+              placeholder='Confirm Password'
+            />
+          </div>
+        </div>
+
+        <button type='submit' className={styles.Button}>
+          <span>Sign Up</span>
+          <ArrowRight size={18} />
+        </button>
+
+        <div className={styles.signWith}>
+          <div className={styles.signWithLine}></div>
+          <p className={styles.signWithText}>Sign up with</p>
+          <div className={styles.signWithLine}></div>
+        </div>
+
+        <ul className={styles.socialsLine}>
+          <li className={styles.social}>
+            <Image
+              src='/icons/icon-fb.svg'
+              alt='Facebook'
+              className={styles.socialIcon}
+              width={24}
+              height={24}
+            />
+            <p style={{ color: '#0866FF' }}>Facebook</p>
+          </li>
+          <li className={styles.social}>
+            <Image
+              src='/icons/icon-google.svg'
+              alt='Google'
+              className={styles.socialIcon}
+              width={24}
+              height={24}
+            />
+            <p style={{ color: '#EA4335' }}>Google</p>
+          </li>
+          <li className={styles.social}>
+            <Image
+              src='/icons/icon-microsoft.svg'
+              alt='Microsoft'
+              className={styles.socialIcon}
+              width={24}
+              height={24}
+            />
+            <p style={{ color: '#000000' }}>Microsoft</p>
+          </li>
+        </ul>
       </div>
     </div>
   )
