55098920428a50b504b75a402eaa52abc37eec49
Egor <visowiacsgo@gmail.com>
2025-03-18 22:16:31 +0100

    make Breadcrumps

--- skillboost-frontend/src/app/categories/page.tsx
+++ skillboost-frontend/src/app/categories/page.tsx
@@ -1,3 +1,22 @@
+import { Breadcrumbs } from "@/shared/ui/breadcrumbs/ui/Breadcrumbs"
+
+const breadcrumbs = [
+  {
+    title: 'HomePage',
+    href: '/'
+  },
+  {
+    title: 'Categories',
+    color: '#2563EB',
+    arrow: true
+  }
+]
+
 export default function CategoriesPage() {
-  return <h1>Categories</h1>
+  return (
+    <>
+        <Breadcrumbs items={breadcrumbs}/>
+        <a href="/details">Go to details page</a>
+    </>
+  )
 }
---
+++ skillboost-frontend/src/app/details/[id]/page.tsx
@@ -0,0 +1,4 @@
+export default function DetailsPage({ params }: { params: { id: string } }) {
+    return <h1>Category: {params.id}</h1>
+  }
+  
