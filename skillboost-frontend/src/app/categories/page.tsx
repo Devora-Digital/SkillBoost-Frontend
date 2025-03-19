7ea66ea8264160202b53de9a97aaf04942049ab4
Dasha <153850235+kowkaLYT@users.noreply.github.com>
2025-03-19 14:47:36 +0200

    Revert "make Breadcrumps"
    This reverts commit 55098920428a50b504b75a402eaa52abc37eec49.

--- skillboost-frontend/src/app/categories/page.tsx
+++ skillboost-frontend/src/app/categories/page.tsx
@@ -1,22 +1,3 @@
-import { Breadcrumbs } from "@/shared/ui/breadcrumbs/ui/Breadcrumbs"
-
-const breadcrumbs = [
-  {
-    title: 'HomePage',
-    href: '/'
-  },
-  {
-    title: 'Categories',
-    color: '#2563EB',
-    arrow: true
-  }
-]
-
 export default function CategoriesPage() {
-  return (
-    <>
-        <Breadcrumbs items={breadcrumbs}/>
-        <a href="/details">Go to details page</a>
-    </>
-  )
+  return <h1>Categories</h1>
 }
--- skillboost-frontend/src/app/details/[id]/page.tsx
+++
@@ -1,4 +0,0 @@
-export default function DetailsPage({ params }: { params: { id: string } }) {
-    return <h1>Category: {params.id}</h1>
-  }
-  
