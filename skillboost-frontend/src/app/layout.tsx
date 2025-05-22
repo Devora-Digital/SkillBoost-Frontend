9dffe1436e86109b3e27944fcf99222c8b7a61a6
cvccvc40 <158179026+cvccvc40@users.noreply.github.com>
2025-05-22 23:52:28 +0300

    Fixed errors in layout.tsx and scss variables

--- skillboost-frontend/src/app/layout.tsx
+++ skillboost-frontend/src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import './styles/globals.scss'
 import { Inter } from 'next/font/google'
-import './globals.css'
 
 const inter = Inter({
   variable: '--font-inter',
