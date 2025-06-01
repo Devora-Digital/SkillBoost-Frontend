18c3b1e2910507c8015cd960fa4ffc23bba865b4
vmditx <vdmitx@gmail.com>
2025-06-01 20:28:13 +0300

    Course Pages start

--- skillboost-frontend/src/app/courses/[id]/page.tsx
+++ skillboost-frontend/src/app/courses/[id]/page.tsx
@@ -1,3 +1,30 @@
-export default function CoursePage({ params }: { params: { id: string } }) {
-  return <h1>Course detail: {params.id}</h1>
+import { Breadcrumbs } from '@/shared/ui/BreadCrumbs/BreadCrumbs'
+import CoursePages from '@/widgets/CoursePage/ui/CoursePages'
+
+
+
+const breadcrumbs = [
+  {
+    title: 'HomePage',
+    href: '/'
+  },
+  {
+    title: 'Categories',
+    color: '#2563EB',
+    arrow: true
+  },
+  {
+    title: 'Categories',
+    color: '#2563EB',
+    arrow: true
+  }
+]
+
+export default function CoursePage() {
+  return (
+    <>
+          <Breadcrumbs items={breadcrumbs} />
+          <CoursePages/>
+        </>
+  )
 }
