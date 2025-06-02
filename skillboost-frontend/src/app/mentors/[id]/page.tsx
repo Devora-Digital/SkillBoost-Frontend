8d735ca513c90391a624a7c5f854c976956d0916
vmditx <vdmitx@gmail.com>
2025-06-02 21:36:41 +0300

    MentorPage upd

--- skillboost-frontend/src/app/mentors/[id]/page.tsx
+++ skillboost-frontend/src/app/mentors/[id]/page.tsx
@@ -1,3 +1,13 @@
+import MentorInfo from "@/widgets/MentorInfo/ui/MentorInfo";
+
+
 export default function MentorPage({ params }: { params: { id: string } }) {
-  return <h1>Mentor detail: {params.id}</h1>
+  return (
+    // <h1>Mentor detail: { params.id }</h1>
+    // console.log(params.id)
+    <MentorInfo/>
+    
+  )
 }
+
+
