41c0742f56821c7608f8ae99d50bbe36aca05b2d
Dasha <153850235+kowkaLYT@users.noreply.github.com>
2025-06-08 13:31:32 +0300

    Fix

--- skillboost-frontend/src/app/page.tsx
+++ skillboost-frontend/src/app/page.tsx
@@ -1,12 +1,5 @@
-import RootLayoutClient from '../app/layoutClient'
 import styles from './page.module.css'
 
 export default function Home() {
-  return (
-    <RootLayoutClient>
-      <div className={styles.page}>
-        {/* <h1>Home page</h1> */}
-      </div>
-    </RootLayoutClient>
-  )
+  return <div className={styles.page}>{/* Your page content here */}</div>
 }
