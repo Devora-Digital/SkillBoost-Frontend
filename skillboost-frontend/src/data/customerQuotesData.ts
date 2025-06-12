42c147bff3407be98cfaa8bf1ad55b79f79cb70c
cvccvc40 <158179026+cvccvc40@users.noreply.github.com>
2025-06-12 15:59:22 +0300

    Edited Customer quotes mock data

---
+++ skillboost-frontend/src/data/customerQuotesData.ts
@@ -0,0 +1,34 @@
+
+export interface customerQuote {
+    quote_text: string
+    author_name: string
+    author_post: string
+}
+
+export const customerQuote: customerQuote[] = [
+    {
+        quote_text: "Byway's tech courses are top-notch! As someone who's always looking to stay ahead in the rapidly evolving tech world, I appreciate the up-to-date content and engaging multimedia.",
+        author_name: "Jane Doe",
+        author_post: "Designer"
+    },
+    {
+        quote_text: "Byway's tech courses are top-notch! As someone who's always looking to stay ahead in the rapidly evolving tech world, I appreciate the up-to-date content and engaging multimedia.",
+        author_name: "Jane Doe",
+        author_post: "Designer"
+    },
+    {
+        quote_text: "Byway's tech courses are top-notch! As someone who's always looking to stay ahead in the rapidly evolving tech world, I appreciate the up-to-date content and engaging multimedia.",
+        author_name: "Jane Doe",
+        author_post: "Designer"
+    },
+    {
+        quote_text: "Byway's tech courses are top-notch! As someone who's always looking to stay ahead in the rapidly evolving tech world, I appreciate the up-to-date content and engaging multimedia.",
+        author_name: "Jane Doe",
+        author_post: "Designer"
+    },
+    {
+        quote_text: "Byway's tech courses are top-notch! As someone who's always looking to stay ahead in the rapidly evolving tech world, I appreciate the up-to-date content and engaging multimedia.",
+        author_name: "Jane Doe",
+        author_post: "Designer"
+    }
+]
