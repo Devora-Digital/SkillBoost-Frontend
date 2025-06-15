817352c14df89287054cbd9a71be229fa6f54735
Dasha <153850235+kowkaLYT@users.noreply.github.com>
2025-06-15 17:21:14 +0100

    Mentor page last fix Daria
    Last fix for Mentor Page (Daria)
    Changes for Reviews Card

--- skillboost-frontend/src/shared/ui/LearnerReviewsCard/LearnerReviewCard.tsx
+++ skillboost-frontend/src/shared/ui/LearnerReviewsCard/LearnerReviewCard.tsx
@@ -1,50 +1,48 @@
-
 import styles from './LearnerReviewsCard.module.scss'
 import Image from 'next/image'
 
-interface ReviewsCardProps{
-    date: string
-    name: string
-    rating: number 
-    text: string
-    avatar: string
+interface ReviewsCardProps {
+  date: string
+  name: string
+  rating: number
+  text: string
+  avatar: string
 }
-const LeanerReviewsCard: React.FC<ReviewsCardProps> = ({
-    date, name, rating, text, avatar
+
+const LearnerReviewsCard: React.FC<ReviewsCardProps> = ({
+  date,
+  name,
+  rating,
+  text,
+  avatar
 }) => {
-
-
-    return (
-        <div className={styles.containerCard}>
-            <div className={styles.containerPhotoName}>
-                <Image
-                    src={avatar}
-                    alt='SkillBoost'
-                    width={50}
-                    height={50}
-                />
-                <p>{name}</p>
-            </div>
-            <div className={styles.containerStarsReviews}>
-                <div className={styles.containerRatingDate}>       
-                    <div className={styles.containerIconStar}>
-                        <Image
-                            src='/icons/icon-star3.svg'
-                            alt='SkillBoost'
-                            width={16}
-                            height={16}
-                            />
-                        <span>{rating}</span> 
-                    </div>
-                    <span> Reviewed on { date }nd March, 2025</span>
-                </div>
-                <div className={styles.containerReviewText}>
-                    <p className={styles.containerText}>{text}</p>
-                </div>
-            </div>
+  return (
+    <div className={styles.containerCard}>
+      <div className={styles.containerPhotoName}>
+        <Image src={avatar} alt='SkillBoost' width={50} height={50} />
+        <p>{name}</p>
+      </div>
+      <div className={styles.containerStarsReviews}>
+        <div className={styles.containerRatingDate}>
+          <div className={styles.containerIconStar}>
+            <Image
+              src='/icons/icon-star3.svg'
+              alt='Star rating'
+              width={18}
+              height={18}
+            />
+            <span className={styles.containerStarText}>{rating}</span>
+          </div>
+          <span className={styles.containerReviewText}>
+            Reviewed on {date}nd March, 2025
+          </span>
         </div>
-    )
+        <div className={styles.containerReviewText}>
+          <p className={styles.containerText}>{text}</p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default LeanerReviewsCard
-
+export default LearnerReviewsCard
