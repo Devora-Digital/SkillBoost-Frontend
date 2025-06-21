5019f947f7a3114215bd44a6eae05c96c018fdd2
cvccvc40 <158179026+cvccvc40@users.noreply.github.com>
2025-06-21 23:59:39 +0300

    Fixed Customer quotes component №9

--- skillboost-frontend/src/shared/ui/CustomerQuotesSecond/CarouselCustomerQuotesSecond/CarouselCustomerQuotesSecond.tsx
+++ skillboost-frontend/src/shared/ui/CustomerQuotesSecond/CarouselCustomerQuotesSecond/CarouselCustomerQuotesSecond.tsx
@@ -1,8 +1,57 @@
 
+import { useEffect } from "react"
+import useEmblaCarousel from "embla-carousel-react"
+import type { EmblaCarouselType } from 'embla-carousel';
+import Image from "next/image"
+// import { LucideChevronLeft, LucideChevronRight } from "lucide-react"
+import QuoteMarkIcon from '@icons/ri_double-quotes-l.svg'
+import DesignerPhoto from "@assets/Designer_image_01.jpeg"
+import { customersQuotes, customerQuote } from "@/data/customerQuotesData"
 import styles from "./CarouselCustomerQuotesSecond.module.scss"
 
-export default function CarouselCustomerQuotesSecond() {
+type EmblaCarouselProps = {
+    groupedQuotes: customerQuote[][];
+    onApiReady?: (api: EmblaCarouselType) => void;
+}
+
+export default function CarouselCustomerQuotesSecond({ groupedQuotes, onApiReady }: EmblaCarouselProps) {
+    const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false })
+    useEffect(() => {
+        if (emblaApi) {
+            onApiReady?.(emblaApi);
+            const slides = document.querySelectorAll(`.${styles.embla__slide}`);
+            console.log('Слайдов в DOM:', slides.length);
+            console.log('Embla carousel инициализирована. Snap list:', emblaApi.scrollSnapList().length);
+        }
+    }, [emblaApi, onApiReady])
     return (
-        <></>
+        <div className={styles.quotes_block_quotes} key={groupedQuotes.length} >
+            <div className={styles.embla__container} ref={emblaRef} >
+                {/* {groupedQuotes.length > 0 && groupedQuotes.map((group, index) => ( */}
+                {groupedQuotes.map((group, index) => (
+                    <div key={index} className={styles.embla__slide} >
+                        {group.map((quote, subIndex) => (
+                            <div key={subIndex} className={styles.quotes_block_quotes_item} >
+                                <Image src={QuoteMarkIcon} alt='Quote mark icon' />
+                                <p className={styles.quotes_block_quote_text} >
+                                  {quote.quote_text}
+                                </p>
+                                <div className={styles.quotes_block_author}>
+                                  <Image
+                                    src={DesignerPhoto}
+                                    className={styles.quotes_block_author_photo}
+                                    alt='Designer photo'
+                                  />
+                                  <div className={styles.quotes_block_author_inside}>
+                                    <p className={styles.quotes_block_author_name}>{quote.author_name}</p>
+                                    <p className={styles.quotes_block_author_post}>{quote.author_post}</p>
+                                  </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
--- skillboost-frontend/src/shared/ui/CustomerQuotesSecond/CustomerQuotesSecond.tsx
+++ skillboost-frontend/src/shared/ui/CustomerQuotesSecond/CustomerQuotesSecond.tsx
@@ -2,10 +2,13 @@
 
 import { useState, useEffect } from "react"
 import useEmblaCarousel from "embla-carousel-react"
-import Image from "next/image"
+import type { EmblaCarouselType } from 'embla-carousel'
+// import EmblaCarouselType from "embla-carousel-react"
+// import Image from "next/image"
+import CarouselCustomerQuotesSecond from "./CarouselCustomerQuotesSecond/CarouselCustomerQuotesSecond"
 import { LucideChevronLeft, LucideChevronRight } from "lucide-react"
-import QuoteMarkIcon from '@icons/ri_double-quotes-l.svg'
-import DesignerPhoto from "@assets/Designer_image_01.jpeg"
+// import QuoteMarkIcon from '@icons/ri_double-quotes-l.svg'
+// import DesignerPhoto from "@assets/Designer_image_01.jpeg"
 import { customersQuotes, customerQuote } from "@/data/customerQuotesData"
 import styles from "./CustomerQuotesSecond.module.scss"
 
@@ -27,18 +30,19 @@ export default function CustomerQuotesSecond() {
     }
     const groupedQuotes = chunkArray(customersQuotesData, 3);
     // const groupedQuotes = chunkArray(customersQuotes, 3);
-    const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true })
-    useEffect(() => {
-        // if (!emblaApi) return;
-        if (!emblaApi || groupedQuotes.length  < 1) return;
-        emblaApi.reInit();
-        console.log('DOM слайдов:', document.querySelectorAll('.embla__slide').length);
-        console.log('snapList:', emblaApi?.scrollSnapList().length);
-        const autoScroll = setInterval(() => {
-            emblaApi.scrollNext()
-        }, 5000)
-        return () => clearInterval(autoScroll)
-      }, [emblaApi, groupedQuotes.length])
+    const [emblaApi, setEmblaApi] = useState<EmblaCarouselType | null>(null)
+    // const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true })
+    // useEffect(() => {
+    //     // if (!emblaApi) return;
+    //     if (!emblaApi || groupedQuotes.length  < 1) return;
+    //     emblaApi.reInit();
+    //     console.log('DOM слайдов:', document.querySelectorAll('.embla__slide').length);
+    //     console.log('snapList:', emblaApi?.scrollSnapList().length);
+    //     const autoScroll = setInterval(() => {
+    //         emblaApi.scrollNext()
+    //     }, 5000)
+    //     return () => clearInterval(autoScroll)
+    //   }, [emblaApi, groupedQuotes.length])
     // }, [emblaApi, groupedQuotes])
     return (
         <div className={styles.quotes_block} >
@@ -56,37 +60,38 @@ export default function CustomerQuotesSecond() {
             </div>
           </div>
             {groupedQuotes.length > 0 && (
-              <div className={styles.quotes_block_quotes} key={groupedQuotes.length} >
-                  <div className={styles.embla__container} ref={emblaRef} >
-                    {/* {groupedQuotes.length > 0 && groupedQuotes.map((group, index) => ( */}
-                    {groupedQuotes.map((group, index) => (
-                        <div key={index} className={styles.embla__slide} >
-                          {group.map((quote, index) => (
-                              <div key={index} className={styles.quotes_block_quotes_item}>
-                                <Image src={QuoteMarkIcon} alt='Quote mark icon' />
-                                <p className={styles.quotes_block_quote_text}>
-                                  &quot;Byway&apos;s tech courses are top-notch! As someone
-                                  who&apos;s always looking to stay ahead in the rapidly evolving
-                                  tech world, I appreciate the up-to-date content and engaging
-                                  multimedia.
-                                </p>
-                                <div className={styles.quotes_block_author}>
-                                  <Image
-                                    src={DesignerPhoto}
-                                    className={styles.quotes_block_author_photo}
-                                    alt='Designer photo'
-                                  />
-                                  <div className={styles.quotes_block_author_inside}>
-                                    <p className={styles.quotes_block_author_name}>Jane Doe</p>
-                                    <p className={styles.quotes_block_author_post}>Designer</p>
-                                  </div>
-                                </div>
-                              </div>
-                          ))}
-                        </div>
-                    ))}
-                  </div>
-              </div>
+              <CarouselCustomerQuotesSecond groupedQuotes={groupedQuotes} onApiReady={setEmblaApi} />
+              // <div className={styles.quotes_block_quotes} key={groupedQuotes.length} >
+              //     <div className={styles.embla__container} ref={emblaRef} >
+              //       {/* {groupedQuotes.length > 0 && groupedQuotes.map((group, index) => ( */}
+              //       {groupedQuotes.map((group, index) => (
+              //           <div key={index} className={styles.embla__slide} >
+              //             {group.map((quote, index) => (
+              //                 <div key={index} className={styles.quotes_block_quotes_item}>
+              //                   <Image src={QuoteMarkIcon} alt='Quote mark icon' />
+              //                   <p className={styles.quotes_block_quote_text}>
+              //                     &quot;Byway&apos;s tech courses are top-notch! As someone
+              //                     who&apos;s always looking to stay ahead in the rapidly evolving
+              //                     tech world, I appreciate the up-to-date content and engaging
+              //                     multimedia.
+              //                   </p>
+              //                   <div className={styles.quotes_block_author}>
+              //                     <Image
+              //                       src={DesignerPhoto}
+              //                       className={styles.quotes_block_author_photo}
+              //                       alt='Designer photo'
+              //                     />
+              //                     <div className={styles.quotes_block_author_inside}>
+              //                       <p className={styles.quotes_block_author_name}>Jane Doe</p>
+              //                       <p className={styles.quotes_block_author_post}>Designer</p>
+              //                     </div>
+              //                   </div>
+              //                 </div>
+              //             ))}
+              //           </div>
+              //       ))}
+              //     </div>
+              // </div>
             )}
         </div>
     )
