e000a7cd449a1f88d9a2b5309fc84cdd31b18ae4
cvccvc40 <158179026+cvccvc40@users.noreply.github.com>
2025-06-22 15:42:16 +0300

    Fixed Customer quotes component №11

--- skillboost-frontend/src/shared/ui/CustomerQuotesSecond/CarouselCustomerQuotesSecond/CarouselCustomerQuotesSecond.tsx
+++ skillboost-frontend/src/shared/ui/CustomerQuotesSecond/CarouselCustomerQuotesSecond/CarouselCustomerQuotesSecond.tsx
@@ -19,52 +19,12 @@ export default function CarouselCustomerQuotesSecond({ groupedQuotes, onApiReady
     const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
     useEffect(() => {
         if (!emblaApi) return;
-        // console.log('♻️ emblaApi инициализирован');
-        // console.log('📌 Snap points:', emblaApi.scrollSnapList().length);
-        //
-        //
-        // const timeout = setTimeout(() => {
-        //     const slides = document.querySelectorAll(`.${styles.embla__slide}`);
-        //     console.log('🧩 Слайдов в DOM:', slides.length);
-            
-        //     const snapCount = emblaApi.scrollSnapList().length;
-        //     console.log('📌 Snap points:', snapCount);
-        
-        //     if (slides.length > 1 && snapCount <= 1) {
-        //       emblaApi.reInit();
-        //       console.log('♻️ Embla пересчитана вручную через reInit()');
-        //     }
-        
-        //     // 👉 Визуальный автоскролл для теста
-        //     setTimeout(() => {
-        //       console.log('⏩ Пробуем scrollTo(1)');
-        //       emblaApi.scrollTo(1);
-        //     }, 1000);
-        //   }, 30);
-        //
-        // return () => clearTimeout(timeout);
-        //
     }, [emblaApi, groupedQuotes.length]);
-    // useEffect(() => {
-    //     if (emblaApi) {
-    //         onApiReady?.(emblaApi);
-    //         const slides = document.querySelectorAll(`.${styles.embla__slide}`);
-    //         console.log('Слайдов в DOM:', slides.length);
-    //         console.log('Embla carousel инициализирована. Snap list:', emblaApi.scrollSnapList().length);
-    //     }
-    // }, [emblaApi, onApiReady])
-    console.log('📦 groupedQuotes.length =', groupedQuotes.length);
-    groupedQuotes.forEach((group, i) =>
-        console.log(`— Слайд ${i + 1}:`, group.map(q => q.author_name))
-    );
     return (
         <div className={styles.quotes_block_quotes} key={groupedQuotes.length} ref={emblaRef} >
-            {/* <div className={styles.embla__container} ref={emblaRef} > */}
             <div className={styles.embla__container} >
-                {/* {groupedQuotes.length > 0 && groupedQuotes.map((group, index) => ( */}
                 {groupedQuotes.map((group, index) => (
                     <div className={styles.embla__slide} key={index} >
-                        <h2>Слайд №{index + 1}</h2>
                         {group.map((quote, subIndex) => (
                             <div key={subIndex} className={styles.quotes_block_quotes_item} >
                                 <Image src={QuoteMarkIcon} alt='Quote mark icon' />
